eb8968400a9368deaebc2db2a85409b87524cafa
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2014-12-02 15:52:19 +0530

    Modified parser to generate entire Usage.scala file. Still have to figure out a good way to construct the test cases automatically.

--- old_system/parser.py
+++ old_system/parser.py
@@ -4,7 +4,9 @@ KEY_IDENTIFIER = "=>"
 OUTPUT_DIRECTORY = "results"
 INPUT_DIRECTORY = "/home/rohit/hg/sleek_hip/examples/working/sleek/"
 COMMENT = "#"
-
+USAGE_PRE_TEMPLATE = "UsagePre.in"
+USAGE_POST_TEMPLATE = "UsagePost.in"
+NEW_LINE = '\n'
 ''' 
 Examples:
 
@@ -21,14 +23,35 @@ withArguments "--elp" storeOutputInDirectory "results" withOutputFileName "sleek
 
 '''
 
+# Function reads in the original script which follows example format above (read-fast-tests.pl)
 def read_script():
 	global script
 	with open(INPUT_FILE_NAME) as f:
 		script = f.readlines()
 
+def read_pre_template():
+	with open(USAGE_PRE_TEMPLATE) as f:
+		pre = f.readlines()
+	pre_string = ''
+	for line in pre:
+		pre_string += line + NEW_LINE
+	return pre_string
+
+def read_post_template():
+	with open(USAGE_POST_TEMPLATE) as f:
+		post = f.readlines()
+	post_string = ''
+	for line in post:
+		post_string += line + NEW_LINE
+	return post_string
+
 def process_script():
+	global output_contents
+	output_contents = read_pre_template()
 	for line in script:
-		process_line(line)
+		output_contents += process_line(line)
+	output_contents += read_post_template()
+	print(output_contents)
 
 def sanitise_input(word):
 	return word.strip("[").strip("]").replace('"', '')
@@ -46,19 +69,27 @@ def parse_expected_output(expected_output):
 def construct_test(file_name, arguments, expected_output):
 	output_file = sanitise_file_name(file_name).replace(".slk", ".out")
 	test_name = sanitise_file_name(file_name).replace(".slk", "Test")
-	print("val " + test_name + " = ")
-	print('new SleekTestCaseBuilder runCommand "sleek" onFile ' + wrap_quotes(INPUT_DIRECTORY + file_name) + ' withArguments ' + wrap_quotes(arguments) + ' storeOutputInDirectory ' + wrap_quotes(OUTPUT_DIRECTORY) + ' withOutputFileName ' + wrap_quotes(output_file) + ' checkAgainst ' + wrap_quotes(expected_output))
-	print(test_name + ".build " + "generateOutput()")
-	print('\n')
+	test = "val " + test_name + " = " + NEW_LINE
+	test += 'new SleekTestCaseBuilder runCommand "sleek" onFile ' + wrap_quotes(INPUT_DIRECTORY + file_name) + ' withArguments ' + wrap_quotes(arguments) + ' storeOutputInDirectory ' + wrap_quotes(OUTPUT_DIRECTORY) + ' withOutputFileName ' + wrap_quotes(output_file) + ' checkAgainst ' + wrap_quotes(expected_output) + '\n'
+	test += test_name + ".build " + "generateOutput()" + NEW_LINE
+	test += NEW_LINE
+	return test
+	# print("val " + test_name + " = ")
+	# print('new SleekTestCaseBuilder runCommand "sleek" onFile ' + wrap_quotes(INPUT_DIRECTORY + file_name) + ' withArguments ' + wrap_quotes(arguments) + ' storeOutputInDirectory ' + wrap_quotes(OUTPUT_DIRECTORY) + ' withOutputFileName ' + wrap_quotes(output_file) + ' checkAgainst ' + wrap_quotes(expected_output))
+	# print(test_name + ".build " + "generateOutput()")
+	# print('\n')
 
 def process_line(line):
 	line = line.strip()
+	test = ''
 	if(KEY_IDENTIFIER not in line and COMMENT not in line and len(line) > 2):
 		components = line.split(',')
 		file_name = sanitise_input(components[0])
 		arguments = sanitise_input(components[1])
 		expected_output = parse_expected_output(sanitise_input(components[3]))
-		construct_test(file_name, arguments, expected_output)
+		test = construct_test(file_name, arguments, expected_output)
+	return test
+
 
 read_script()
 process_script()
