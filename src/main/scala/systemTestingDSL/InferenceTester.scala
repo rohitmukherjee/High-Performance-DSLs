b7028ed7488a453a2e9d31dfd5626a3530391c95
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2014-12-23 20:59:46 +0530

    Work In Progress. Added unit test for checkCorpus method in trait

--- src/main/scala/systemTestingDSL/InferenceTester.scala
+++ src/main/scala/systemTestingDSL/InferenceTester.scala
@@ -14,16 +14,22 @@ trait InferenceTester {
     val expectedResult: String = expected._1
     val expectedResidue: String = expected._2
     def validFormat: Boolean = entail.contains(":") && entail.contains(".") && entail.contains("Residue:")
-    def matchResult: Boolean = entail.substring(entail.indexOf(":") + 1, entail.indexOf(".")).trim equals expectedResult
-    def matchResidue: Boolean = entail.substring(entail.indexOf("Residue:")).trim() equals expectedResidue
+    def matchResult: Boolean = entail.substring(entail.indexOf(":") + 1, entail.indexOf(".")).trim equals expectedResult.trim
+    def matchResidue: Boolean = entail.substring(entail.indexOf("Residue:")).trim() equals expectedResidue.trim()
     if (validFormat)
       matchResult & matchResidue
     else validFormat
   }
 
   def checkCorpus(corpus: String, results: Seq[(String, String)]): Boolean = {
-    def sanitizeCorpus: String = corpus.substring(0, corpus.indexOf("Stop")).substring(corpus.indexOf("Entail"))
-    val splitCorpus: Array[String] = sanitizeCorpus.split("Entail\\s\\d\\d:\\s.*Valid.*|Entail\\s\\d\\d:\\s.*Fail.*\\n")
+    def sanitizeCorpus: String = corpus.substring(0, corpus.indexOf("Stop")).substring(corpus.indexOf("Entail")).replace("\n\n", "\n").replace("\t", "").trim
+    val splitCorpus: Array[String] = sanitizeCorpus.split("\n\n")
+    println("Corpus length: " + splitCorpus.length)
+    println("result length: " + results.length)
+    println("Corpus")
+    val zipped = splitCorpus.zipWithIndex
+    //Broken need to fix
+    // zipped.forall(case (index, tuple) => entailCheck(tuple._1, tuple._2 ))
     splitCorpus.length == results.length
   }
 }
