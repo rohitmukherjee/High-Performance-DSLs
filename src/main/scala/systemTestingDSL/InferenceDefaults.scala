4c354b8b06e500b0747135193eda890d50a01bea
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2015-01-15 01:21:36 +0800

    Cleaned up tests and removed all "Residue: " Prefixes

--- src/main/scala/systemTestingDSL/InferenceDefaults.scala
+++ src/main/scala/systemTestingDSL/InferenceDefaults.scala
@@ -1,7 +1,7 @@
 package systemTestingDSL
 
 object InferenceDefaults {
-  
+
   val FAIL: String = "Fail"
 
   val VALID: String = "Valid"
@@ -14,8 +14,12 @@ object InferenceDefaults {
 
   def removeWhiteSpaceCharacters(text: String): String = text.replace("\n", "").replace("\t", "").replace(" ", "").trim()
 
-  def NEW_LINE: String = "\n"
+  val NEW_LINE: String = "\n"
 
-  def DOUBLE_NEW_LINE: String = NEW_LINE.concat(NEW_LINE)
+  val DOUBLE_NEW_LINE: String = NEW_LINE.concat(NEW_LINE)
+
+  val TRIPLE_NEW_LINE: String = NEW_LINE.concat(NEW_LINE).concat(NEW_LINE)
+
+  val ANGLE_OPEN: String = "<"
 
 }
--- src/main/scala/systemTestingDSL/InferenceTester.scala
+++ src/main/scala/systemTestingDSL/InferenceTester.scala
@@ -2,29 +2,41 @@ package systemTestingDSL
 
 trait InferenceTester {
 
-  def isFail(result: String): Boolean = result.equals(InferenceDefaults.FAIL)
+  def isFail(result: String): Boolean = result.equalsIgnoreCase(InferenceDefaults.FAIL)
 
-  def isValid(result: String): Boolean = result.equals(InferenceDefaults.VALID)
+  def isValid(result: String): Boolean = result.equalsIgnoreCase(InferenceDefaults.VALID)
 
   def entailCheck(entail: String, expected: (String, String)): Boolean = {
+    //        println(entail)
     val expectedResult: String = expected._1
     val expectedResidue: String = expected._2
-    def validFormat: Boolean = entail.contains(InferenceDefaults.ENTAIL) && entail.contains(":") && entail.contains(".") && entail.contains(InferenceDefaults.RESIDUE)
-    lazy val matchResult: Boolean = InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf(":") + 1, entail.indexOf("."))) equals InferenceDefaults.removeWhiteSpaceCharacters(expectedResult)
-    lazy val matchResidue: Boolean = InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf(InferenceDefaults.RESIDUE) + InferenceDefaults.RESIDUE.length())) equals InferenceDefaults.removeWhiteSpaceCharacters(expectedResidue)
+    def validFormat: Boolean = entail.contains(InferenceDefaults.ENTAIL) && entail.contains(":") && entail.contains(".")
+    val actualResult: String = InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf(":") + 1, entail.indexOf(".")))
+    lazy val matchResult: Boolean = actualResult equals InferenceDefaults.removeWhiteSpaceCharacters(expectedResult)
+    lazy val matchResidue: Boolean = InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf(InferenceDefaults.ANGLE_OPEN))) equals InferenceDefaults.removeWhiteSpaceCharacters(expectedResidue)
     if (validFormat) {
-//            println(matchResult)
-//            println(matchResidue)
-//            println("actual: " + InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf("Residue:"))))
-//            println("expected: " + InferenceDefaults.removeWhiteSpaceCharacters(expectedResidue))
-      matchResult & matchResidue
-    } else validFormat
+      if (matchResult) {
+        if (isFail(actualResult)) {
+          //          println("Result: Fail")
+          return true
+        }
+        if (isValid(actualResult)) {
+          //          println("Result: Valid")
+          //          println("actual: " + InferenceDefaults.removeWhiteSpaceCharacters(entail.substring(entail.indexOf(InferenceDefaults.ANGLE_OPEN))))
+          //          println("Residue Match Result:" + matchResidue)
+          //          println("expected: " + InferenceDefaults.removeWhiteSpaceCharacters(expectedResidue))
+          return matchResidue
+        }
+      }
+      return false
+    } else return validFormat
   }
 
   def checkCorpus(corpus: String, results: Seq[(String, String)]): Boolean = {
     if (corpus.length() == 0 || results.length == 0)
       return false
     lazy val sanitizeCorpus: String = corpus.substring(0, corpus.indexOf(InferenceDefaults.STOP)).substring(corpus.indexOf(InferenceDefaults.ENTAIL)).replace(InferenceDefaults.DOUBLE_NEW_LINE, InferenceDefaults.NEW_LINE).replace("\t", "").trim
+    //    println (sanitizeCorpus)
     val splitCorpus: Array[String] = sanitizeCorpus.split(InferenceDefaults.DOUBLE_NEW_LINE)
     //    println("Corpus length: " + splitCorpus.length)
     //    println("result length: " + results.length)
