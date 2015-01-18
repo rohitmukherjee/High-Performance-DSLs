113ed03e316ee12685d407f29dc1ef1106d4cd2e
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2015-01-18 13:01:55 +0800

    Now iterates through branches and prints

--- Reporting Tool/hg_api.py
+++ Reporting Tool/hg_api.py
@@ -1,4 +1,5 @@
 import os
+import time
 import settings
 import hg_commands as commands
 
@@ -15,6 +16,30 @@ def list_all_branches():
 def pull():
 	execute(commands.pull)
 
-set_directory()
-print(list_all_branches())
-pull()
+def checkout_branch(branch_name):
+	execute(commands.update + " " + branch_name)
+
+def process_branch(branch_name):
+	try:
+		checkout_branch(branch_name)
+		if check_last_commit_date():
+			print("Running tests on %s" % branch_name)
+	except:
+		print("Error processing branch %s" % branch_name)
+
+def last_commit_date():
+	return execute(commands.last_commit_date).split()[0]
+
+def check_last_commit_date():
+	current_milli_time = lambda: int(time.time())
+	# print(current_milli_time())
+	# print(int(last_commit_date()))
+	return (current_milli_time() - int(last_commit_date()) <= settings.app['time_period'])
+
+def run():
+	set_directory()
+	branches = list_all_branches().split("\n")
+	pull()
+	for branch in branches:
+		process_branch(branch)
+run()
