97c105096df76b66e8aa9760b889d15ec79ff0ad
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2015-01-28 14:08:42 +0800

    works

--- Reporting Tool/main.py
+++ Reporting Tool/main.py
@@ -11,7 +11,7 @@ ONE_DAY = 24 * 60 * 60
 
 def create_directory(directory):
 	if not os.path.exists(directory):
-	    os.makedirs(directory)
+         os.makedirs(directory)
 
 def ensure_output_directory_exists():
 	create_directory(settings['app']['output_directory_location'] + settings['app']['output_directory_name'])
@@ -60,15 +60,16 @@ def get_output_file_name(branch_name):
 	return settings['test']['prefix'] + branch_name + settings['test']['file_extension']
 
 def run_test(commit_hash, branch_name):
-	cwd = os.getcwd()
-	print("Currently in: " + cwd)
-	os.chdir(settings['test']['directory'])
-	output_file_name = get_output_file_name(commit_hash)
-	output = open(settings['app']['output_directory_location'] + settings['app']['output_directory_name'] + '/' + branch_name + '/' + commit_hash, 'w+')
-	print("Running Test on branch %s" % branch_name)
-	handle = subprocess.call([settings['test']['command']], shell = True, stdout = output)
-	print("Output stored in directory %s/%s" % (commit_hash, output_file_name))
-	os.chdir(cwd)
+    cwd = os.getcwd()
+    print("Currently in: " + cwd)
+    os.chdir(settings['test']['directory'])
+    output_file_name = settings['app']['output_directory_location'] + settings['app']['output_directory_name'] + '/' + branch_name + '/' + commit_hash + '.out'
+    if not os.path.isfile(output_file_name):
+        output = open(output_file_name, 'w+')
+        print("Running Test on commit %s" % commit_hash)
+        handle = subprocess.call([settings['test']['command']], shell = True, stdout = output)
+        print("Output stored in directory %s/%s" % (commit_hash, output_file_name))
+        os.chdir(cwd)
 
 def run():
 	setup()
