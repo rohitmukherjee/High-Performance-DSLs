0142b11246096d4d1076d1f8368dd7c8fe67e93e
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2015-03-23 21:20:18 +0800

    Made reference test builder more generic

--- src/main/scala/systemTestingDSL/RegressionTestReferenceBuilder.scala
+++ src/main/scala/systemTestingDSL/RegressionTestReferenceBuilder.scala
@@ -1,25 +1,31 @@
 package systemTestingDSL
 
 import com.typesafe.config.Config
-import systemTestingDSL.testSuite.HipTestSuite
 import scala.collection.mutable.ArrayBuffer
+import collection.JavaConversions._
 
 class RegressionTestReferenceBuilder(configuration: Config) {
 
-  private def getFileList(directory: String): Array[String] = {
-    FileSystemUtilities.getRecursiveListOfFilesWithRegex(directory, ".ss").map(_.getAbsolutePath())
+  private def getFileList(directory: String, extension: String): Array[String] = {
+    FileSystemUtilities.getRecursiveListOfFilesWithRegex(directory, extension).map(_.getAbsolutePath())
   }
 
-  def buildHipTests(): ArrayBuffer[GenericTestCase] = {
-    val files = getFileList(configuration.getString("HIP_REF_SOURCE_DIRECTORY"))
-    val outputDirectory = configuration.getString("HIP_REF_OUTPUT_DIRECTORY")
+  def buildTests(): ArrayBuffer[GenericTestCase] = {
+    val refTests = configuration.getConfigList("REFERENCE_TESTS")
+    println(refTests)
     val referenceRuns = ArrayBuffer[GenericTestCase]()
-    files.foreach(file => referenceRuns += new GenericTestCase("hip", file, "", outputDirectory, file.substring(file.lastIndexOf("/") + 1)))
+    for (configuration <- refTests) {
+      val files = getFileList(configuration.getString("SOURCE_DIRECTORY"), configuration.getString("EXTENSION"))
+      val outputDirectory = configuration.getString("REF_OUTPUT_DIRECTORY")
+      val commandName = configuration.getString("COMMAND_NAME")
+      val arguments = configuration.getString("ARGUMENTS")
+      files.foreach(file => referenceRuns += new GenericTestCase(commandName, file, "", outputDirectory, file.substring(file.lastIndexOf("/") + 1)))
+    }
     referenceRuns
   }
 
   def run(): Unit = {
-    val references = this.buildHipTests()
-    references.foreach(_.run)
+    val references = this.buildTests()
+        references.foreach(_.run)
   }
 }
