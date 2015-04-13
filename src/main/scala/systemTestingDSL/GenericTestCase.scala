c8f37de88d963897629f41ce75542bacfa8a4988
Rohit Mukherjee <mukherjeerohit93@gmail.com>
2015-04-13 22:56:13 +0800

    almost finished svcomp functionality

--- src/main/scala/systemTestingDSL/GenericTestCase.scala
+++ src/main/scala/systemTestingDSL/GenericTestCase.scala
@@ -2,11 +2,24 @@ package systemTestingDSL
 
 import systemTestingDSL.outputGenerator.ConsoleOutputGenerator
 
-case class GenericTestCase(commandName: String, fileName: String, arguments: String, outputDirectoryName: String, outputFileName: String, outputFileExtension: String = ".out")
+case class GenericTestCase(commandName: String,
+  fileName: String,
+  arguments: String,
+  outputDirectoryName: String = "",
+  outputFileName: String = "",
+  outputFileExtension: String = ".out")
     extends Runnable with ConsoleOutputGenerator {
   var output: (String, Long) = ("", 0)
 
-  def run() = {
+  /**
+   * *
+   * This function does not write to file but just returns the console output
+   */
+  def runAndReturn(): String = {
+    this.execute._1
+  }
+
+  def run(): Unit = {
     this.output = this.execute
     if (outputFileName.length > 0)
       writeToFile(this.outputFileName, this.outputDirectoryName, output._1, outputFileExtension)
--- src/main/scala/systemTestingDSL/SVCompTestSuite.scala
+++ src/main/scala/systemTestingDSL/SVCompTestSuite.scala
@@ -1,15 +1,22 @@
 package systemTestingDSL
 
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.ArrayBuffer
+import systemTestingDSL.outputGenerator.ConsoleOutputGenerator
+import java.io.PrintWriter
 
 case class SVCompTestSuite(directory: String,
+    commandName: String = "hip",
     arguments: String = """-infer "@term" --svcomp-compete""",
-    fileType: String = ".c") extends GetFileList {
+    fileType: String = ".c",
+    printer: PrintWriter = new PrintWriter(System.out, true)) extends GetFileList with ConsoleOutputGenerator {
+  var tests = new HashMap[String, GenericTestCase]
 
   def buildResultMap(): HashMap[String, String] = {
     val files = getFileList(directory, fileType).filter(x => x.matches(".*true.*|.*false.*|.*unknown.*"))
     var resultMap = new HashMap[String, String]()
     files.foreach(file => resultMap.put(file, getResultFromFileName(extractFileNameFromPath(file))))
+    files.foreach(file => tests.put(file, new GenericTestCase(commandName = this.commandName, arguments = this.arguments, fileName = file)))
     resultMap
   }
   def getResultFromFileName(fileName: String): String = {
@@ -25,7 +32,19 @@ case class SVCompTestSuite(directory: String,
   }
 
   def runAllTests(): Unit = {
-    buildResultMap().foreach(println(_))
+    var result = ""
+    buildResultMap().foreach(expectedResult => {
+      val actualResult = tests.get(expectedResult._1).get.runAndReturn
+      result += log(expectedResult._1)
+      if (actualResult.equals(expectedResult._2))
+        result += success("Passed")
+      else {
+        result += error("Failed")
+        result += expected(expectedResult._2)
+        result += actual(actualResult)
+      }
+    })
+    printer.println(result)
   }
 
 }
